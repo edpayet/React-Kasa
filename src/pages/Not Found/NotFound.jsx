565bc4930f3a6f9d4d78b0a812790785c2e030c9
Eddy Briand <333ebr@gmail.com>
2023-01-24 02:49:04 +0100

    feat: 404 page style

--- src/pages/Not Found/NotFound.jsx
+++ src/pages/Not Found/NotFound.jsx
@@ -1,17 +1,34 @@
 import { useRouteError, Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
+import '../Root/root.css';
+import Header from '../../components/Header/Header';
 
 export default function NotFound() {
   const error = useRouteError();
   console.error(error);
 
+  // function showError(error) {
+  //   return (
+  //     <p>
+  //       <i>{error.statusText || error.message}</i>
+  //     </p>
+  //   );
+  // }
+
   return (
-    <div className="NotFound">
-      <h1>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-      <Link to={`/`}>Retourner sur la page d'acceuil</Link>
-    </div>
+    <>
+      <Header />
+      <main>
+        <div className={styles.notFound}>
+          <h1 className={styles.errorCode}>404</h1>
+          <h1 className={styles.errorMessage}>
+            Oups! La page que vous demandez n'existe pas.
+          </h1>
+          <Link to={`/`} className={styles.redirectLink}>
+            Retourner sur la page d'acceuil
+          </Link>
+        </div>
+      </main>
+    </>
   );
 }
